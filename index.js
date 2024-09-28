1f88e55444f2e24b219460e8f8eab0252190b09f
Ch_babula <chinthanippulababulal@gmail.com>
2024-09-28 21:51:03 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const AuthRoutes = require('./routes/AuthRoutes')
+const AdminRoutes = require('./routes/AdminRoutes')
+const EventRoutes = require('./routes/EventRoutes')
+
+/**
+ * APP
+ */
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:5173', 'https://event-spark-chi.vercel.app']
+})) 
+
+
+
+/**
+ * DATABASE
+ */
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connection.once('connected', () => console.log('DATABASE CONNECTED'));
+mongoose.connection.on('error', (er) => console.log("DATABASE ERROR :", er))
+
+
+/**
+ * ROUTES
+ */
+app.use('/health', (req, res) => res.send('Event Spark Backend is healthy'))
+app.use('/api/v1/auth', AuthRoutes);
+app.use('/api/v1/admin', AdminRoutes);
+app.use('/api/v1/event', EventRoutes);
+
+
+/**
+ * LISTEN
+ */
+app.listen(process.env.APP_PORT, () => console.log(`APP LISTENING ON PORT :${process.env.APP_PORT}`))
---
+++ routes/AuthRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const {register, login, profileCheck, logoutUser} = require('../controllers/AuthControllers')
+
+router.post('/register', register)
+router.post('/login', login)
+router.get('/profile', profileCheck)
+router.post('/logout', logoutUser)
+
+module.exports = router;
