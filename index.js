81ee57b4d1e89a79d41afd1741da48467700aa86
Ch_babula <chinthanippulababulal@gmail.com>
2024-09-28 22:49:42 +0530

    bug

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'https://event-spark-chi.vercel.app']
+    origin: ['http://localhost:5173', 'https://event-spark-fe.vercel.app']
 })) 
 
 /**
